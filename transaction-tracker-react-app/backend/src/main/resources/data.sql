792bd144ede19a5fcdb0e1a4ca61a3cdc03064ef
sargarpramod <sargarpramod@gmail.com>
2026-01-06 22:19:42 +0530

    H2 database related changes.

--- transaction-tracker-react-app/backend/src/main/resources/data.sql
+++ transaction-tracker-react-app/backend/src/main/resources/data.sql
@@ -1,96 +1,60 @@
-INSERT INTO categories (name) VALUES ('Household & Utilities'),('EMI'),('Investments'),('Miscellaneous'),
-                                             ('Health & Personal Care'),('Education'),('Quick Commerce'),('Food'),
-                                             ('Lifestyle & Entertainment'),('Credit');
+MERGE INTO "PUBLIC"."CATEGORIES" (ID, NAME, DESCRIPTION)
+    KEY(ID)
+    VALUES
+        (1, 'Household & Utilities', NULL),
+        (2, 'EMI', NULL),
+        (3, 'Investments', NULL),
+        (4, 'Miscellaneous', NULL),
+        (5, 'Health & Personal Care', NULL),
+        (6, 'Education', NULL),
+        (7, 'Quick Commerce', NULL),
+        (8, 'Food', NULL),
+        (9, 'Lifestyle & Entertainment', NULL),
+        (10, 'Credit', NULL);
 
--- Household & Utilities (updated subcategories)
-    INSERT INTO subcategories (category_id, name) VALUES
-    ((SELECT id FROM categories WHERE name = 'Household & Utilities'), 'Groceries(offline)'),
-    ((SELECT id FROM categories WHERE name = 'Household & Utilities'), 'Vegetables/Fruits(offline)'),
-    ((SELECT id FROM categories WHERE name = 'Household & Utilities'), 'Utilities'),
-    ((SELECT id FROM categories WHERE name = 'Household & Utilities'), 'Society Maintenance'),
-    ((SELECT id FROM categories WHERE name = 'Household & Utilities'), 'House Help'),
-    ((SELECT id FROM categories WHERE name = 'Household & Utilities'), 'Transport'),
-    ((SELECT id FROM categories WHERE name = 'Household & Utilities'), 'Service/Repairs'),
-    ((SELECT id FROM categories WHERE name = 'Household & Utilities'), 'Adhoc');
-
--- EMI, Investments, Miscellaneous (single entry categories)
-INSERT INTO subcategories (category_id, name) VALUES
-      ((SELECT id FROM categories WHERE name = 'EMI'), 'EMI'),
-      ((SELECT id FROM categories WHERE name = 'Investments'), 'Investments'),
-      ((SELECT id FROM categories WHERE name = 'Miscellaneous'), 'Others');
-
--- Health & Personal Care
-INSERT INTO subcategories (category_id, name) VALUES
-      ((SELECT id FROM categories WHERE name = 'Health & Personal Care'), 'Medicial'),
-      ((SELECT id FROM categories WHERE name = 'Health & Personal Care'), 'Saloon'),
-      ((SELECT id FROM categories WHERE name = 'Health & Personal Care'), 'Skin Care products'),
-      ((SELECT id FROM categories WHERE name = 'Health & Personal Care'), 'Doctor Visits'),
-      ((SELECT id FROM categories WHERE name = 'Health & Personal Care'), 'Vaccination');
-
--- Education
-INSERT INTO subcategories (category_id, name) VALUES
-      ((SELECT id FROM categories WHERE name = 'Education'), 'Stationary'),
-      ((SELECT id FROM categories WHERE name = 'Education'), 'School Activities Fees'),
-      ((SELECT id FROM categories WHERE name = 'Education'), 'School fees'),
-      ((SELECT id FROM categories WHERE name = 'Education'), 'Tuition fees');
-
--- Quick Commerce (NEW Category)
-INSERT INTO subcategories (category_id, name) VALUES
-      ((SELECT id FROM categories WHERE name = 'Quick Commerce'), 'Zepto'),
-      ((SELECT id FROM categories WHERE name = 'Quick Commerce'), 'Swiggy'),
-      ((SELECT id FROM categories WHERE name = 'Quick Commerce'), 'BBDaily'),
-      ((SELECT id FROM categories WHERE name = 'Quick Commerce'), 'Others');
-
--- Food (updated subcategories)
-INSERT INTO subcategories (category_id, name) VALUES
-      ((SELECT id FROM categories WHERE name = 'Food'), 'Zomato/Swiggy'),
-      ((SELECT id FROM categories WHERE name = 'Food'), 'Prashant Corner'),
-      ((SELECT id FROM categories WHERE name = 'Food'), 'Restaurants/Dine In'),
-      ((SELECT id FROM categories WHERE name = 'Food'), 'Egg/Mutton');
-
--- Lifestyle & Entertainment
-INSERT INTO subcategories (category_id, name) VALUES
-      ((SELECT id FROM categories WHERE name = 'Lifestyle & Entertainment'), 'Clothes'),
-      ((SELECT id FROM categories WHERE name = 'Lifestyle & Entertainment'), 'Electronics'),
-      ((SELECT id FROM categories WHERE name = 'Lifestyle & Entertainment'), 'Accessories'),
-      ((SELECT id FROM categories WHERE name = 'Lifestyle & Entertainment'), 'Entertainment'),
-      ((SELECT id FROM categories WHERE name = 'Lifestyle & Entertainment'), 'Travel (Domestic, International)'),
-      ((SELECT id FROM categories WHERE name = 'Lifestyle & Entertainment'), 'Hotel Stay'),
-      ((SELECT id FROM categories WHERE name = 'Lifestyle & Entertainment'), 'Car Service'),
-      ((SELECT id FROM categories WHERE name = 'Lifestyle & Entertainment'), 'Car Maintenance'),
-      ((SELECT id FROM categories WHERE name = 'Lifestyle & Entertainment'), 'Bike Repairs'),
-      ((SELECT id FROM categories WHERE name = 'Lifestyle & Entertainment'), 'Fuel');
-
-INSERT INTO subcategories (category_id, name) VALUES
-      ((SELECT id FROM categories WHERE name = 'Credit'), 'Salary'),
-      ((SELECT id FROM categories WHERE name = 'Credit'), 'Dividends from stocks'),
-      ((SELECT id FROM categories WHERE name = 'Credit'), 'FD Sweeps'),
-      ((SELECT id FROM categories WHERE name = 'Credit'), 'Other Income');
-
-INSERT INTO payee_category_mapping (payee_name, category_id, subcategory_id)
-VALUES ('SWIGGY', 8, 1);
-
-INSERT INTO payee_category_mapping (payee_name, category_id, subcategory_id)
-VALUES ('ZOMATO', 8, 1);
-
-INSERT INTO payee_category_mapping (payee_name, category_id, subcategory_id)
-VALUES ('AMAZON', 9, 3);
-
-INSERT INTO payee_category_mapping (payee_name, category_id, subcategory_id)
-VALUES ('RASECC THANE', 2, 1);
-
-// important queries
--- investment
-SELECT *
-FROM transactions
-WHERE MONTH(txn_date) = 10
-  AND YEAR(txn_date) = 2025
-  and category_id=3;
-
--- Income
-
-SELECT *
-FROM transactions
-WHERE MONTH(txn_date) = 10
-  AND YEAR(txn_date) = 2025
-  and category_id=10;
+MERGE INTO "PUBLIC"."SUBCATEGORIES" (ID, CATEGORY_ID, NAME, DESCRIPTION)
+    KEY(ID)
+    VALUES
+        (1, 1, 'Groceries(offline)', NULL),
+        (2, 1, 'Vegetables/Fruits(offline)', NULL),
+        (3, 1, 'Utilities', NULL),
+        (4, 1, 'Society Maintenance', NULL),
+        (5, 1, 'House Help', NULL),
+        (6, 1, 'Transport', NULL),
+        (7, 1, 'Service/Repairs', NULL),
+        (8, 1, 'Adhoc', NULL),
+        (9, 2, 'EMI', NULL),
+        (10, 3, 'Investments', NULL),
+        (11, 4, 'Others', NULL),
+        (12, 5, 'Medicial', NULL),
+        (13, 5, 'Saloon', NULL),
+        (14, 5, 'Skin Care products', NULL),
+        (15, 5, 'Doctor Visits', NULL),
+        (16, 5, 'Vaccination', NULL),
+        (17, 6, 'Stationary', NULL),
+        (18, 6, 'School Activities Fees', NULL),
+        (19, 6, 'School fees', NULL),
+        (20, 6, 'Tuition fees', NULL),
+        (21, 7, 'Zepto', NULL),
+        (22, 7, 'Swiggy', NULL),
+        (23, 7, 'BBDaily', NULL),
+        (24, 7, 'Others', NULL),
+        (25, 8, 'Zomato/Swiggy', NULL),
+        (26, 8, 'Prashant Corner', NULL),
+        (27, 8, 'Restaurants/Dine In', NULL),
+        (28, 9, 'Clothes', NULL),
+        (29, 9, 'Electronics', NULL),
+        (30, 9, 'Accessories', NULL),
+        (31, 9, 'Entertainment', NULL),
+        (32, 9, 'Travel (Domestic, International)', NULL),
+        (33, 9, 'Hotel Stay', NULL),
+        (34, 9, 'Car Service', NULL),
+        (35, 9, 'Car Maintenance', NULL),
+        (36, 9, 'Bike Repairs', NULL),
+        (37, 9, 'Fuel', NULL),
+        (38, 10, 'Salary', NULL),
+        (39, 10, 'Dividends from stocks', NULL),
+        (40, 10, 'FD Sweeps', NULL),
+        (41, 10, 'Other Income', NULL),
+        (42, 6, 'Upskilling', NULL),
+        (43, 9, 'Birthday celebrations', NULL);
