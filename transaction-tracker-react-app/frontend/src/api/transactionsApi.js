76814803fa5da03458f32a7a5dbf2f071dc96406
sargarpramod <sargarpramod@gmail.com>
2026-01-28 20:49:59 +0530

    Code Refactor.

---
+++ transaction-tracker-react-app/frontend/src/api/transactionsApi.js
@@ -0,0 +1,59 @@
+import config from "../config/config";
+
+export const saveTransactions = async (aggregatedData) => {
+  const jwt = localStorage.getItem("jwt");
+
+  const response = await fetch(`${config.API_BASE}/api/save-transactions`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": `Bearer ${jwt}`
+    },
+    body: JSON.stringify(aggregatedData, null, 2)
+  });
+
+  if (!response.ok) {
+    throw new Error("Error in data save");
+  }
+
+  return response.text();
+};
+
+export const fetchTransactions = async () => {
+  const jwt = localStorage.getItem("jwt");
+  const response = await fetch(`${config.API_BASE}/api/transactions`, {
+    headers: { "Authorization": `Bearer ${jwt}` }
+  });
+  if (!response.ok) throw new Error("Error fetching transactions");
+  return response.json();
+};
+
+export const fetchTransactionCategories = async () => {
+  const jwt = localStorage.getItem("jwt");
+  const response = await fetch(`${config.API_BASE}/api/transaction-categories`, {
+           method: "GET",
+           headers: {
+             "Authorization": `Bearer ${jwt}`,   // Attach JWT here
+             "Content-Type": "application/json"
+           }
+         })
+  if (!response.ok) throw new Error("Error fetching transactions");
+  return response.json();
+};
+
+export const uploadTransactions = async (uploadedFile) => {
+  const formData = new FormData();
+  formData.append("file", uploadedFile);
+  const jwt = localStorage.getItem("jwt");
+  console.log('config.API_BASE')
+  console.log(config.API_BASE)
+   const response = await fetch(`${config.API_BASE}/api/upload-transaction-file`, {
+            method: "POST",
+            headers: {
+                                  'Authorization': `Bearer ${jwt}`
+                              },
+            body: formData,
+          });
+  if (!response.ok) throw new Error("File upload failed");
+  return response.json();
+};
