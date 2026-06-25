300e34cd9110c79f750679eeaaecd4546831cad5
sargarpramod-rgb <pramodsargar1234@gmail.com>
2026-06-25 21:31:38 +0530

    Update 6_alb.tf

--- transaction-tracker-react-app/infra/6_alb.tf
+++ transaction-tracker-react-app/infra/6_alb.tf
@@ -1,79 +1,133 @@
-# Target Group
+############################################
+# VARIABLES
+############################################
+
+variable "vpc_id" {
+  description = "VPC ID where ALB will be created"
+  type        = string
+}
+
+variable "ec2_instance_id" {
+  description = "EC2 instance ID to attach"
+  type        = string
+}
+
+variable "alb_security_group_id" {
+  description = "Security group for ALB"
+  type        = string
+}
+
+variable "certificate_arn" {
+  description = "ACM certificate ARN (MUST be in same region as ALB)"
+  type        = string
+}
+
+############################################
+# DATA SOURCES
+############################################
+
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [var.vpc_id]
+  }
+}
+
+############################################
+# TARGET GROUP
+############################################
+
 resource "aws_lb_target_group" "transaction_tracker_tg" {
-  name     = "TransactionTrackerTargetGroup"
+  name     = "transaction-tracker-tg"
   port     = 8080
   protocol = "HTTP"
-  vpc_id   = "vpc-00f38496cab1adde6"
+  vpc_id   = var.vpc_id
 
   health_check {
-    protocol            = "HTTP"
-    port                = "traffic-port"
     path                = "/actuator/health"
+    protocol            = "HTTP"
     interval            = 30
     timeout             = 5
-    healthy_threshold   = 5
+    healthy_threshold   = 2
     unhealthy_threshold = 2
     matcher             = "200"
   }
 
-  target_type      = "instance"
-  protocol_version = "HTTP1"
-  ip_address_type  = "ipv4"
-}
-
-# Get the default VPC
-data "aws_vpc" "default" {
-  default = true
-}
-
-# Get all default subnets in that VPC
-data "aws_subnets" "default" {
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.default.id]
-  }
-}
-
-# Application Load Balancer using default subnets
-resource "aws_lb" "transaction_tracker_alb" {
-  name               = "TranscationTrackerALB"
-  internal           = false
-  load_balancer_type = "application"
-  security_groups    = ["sg-032e06887c0406d25"]
-
-  # Automatically use all default subnets
-  subnets            = data.aws_subnets.default.ids
-
-  ip_address_type    = "ipv4"
+  target_type = "instance"
 
   tags = {
-    Name = "TranscationTrackerALB"
+    Name = "transaction-tracker-tg"
   }
 }
 
-# HTTPS Listener
+############################################
+# APPLICATION LOAD BALANCER
+############################################
+
+resource "aws_lb" "transaction_tracker_alb" {
+  name               = "transaction-tracker-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [var.alb_security_group_id]
+  subnets            = data.aws_subnets.default.ids
+
+  tags = {
+    Name = "transaction-tracker-alb"
+  }
+}
+
+############################################
+# HTTP LISTENER (REDIRECT TO HTTPS)
+############################################
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.transaction_tracker_alb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type = "redirect"
+
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+}
+
+############################################
+# HTTPS LISTENER
+############################################
+
 resource "aws_lb_listener" "https" {
   load_balancer_arn = aws_lb.transaction_tracker_alb.arn
   port              = 443
   protocol          = "HTTPS"
-  ssl_policy        = "ELBSecurityPolicy-TLS13-1-2-Res-PQ-2025-09"
 
-  certificate_arn   = "arn:aws:acm:eu-north-1:316502580034:certificate/738ecd59-8c13-4ca7-855f-c4192301bd07"
+  ssl_policy      = "ELBSecurityPolicy-2016-08"
+  certificate_arn = var.certificate_arn
 
   default_action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.transaction_tracker_tg.arn
-
-    forward {
-      target_group {
-        arn    = aws_lb_target_group.transaction_tracker_tg.arn
-        weight = 1
-      }
-
-      stickiness {
-        enabled  = false
-        duration = 3600
-      }
-    }
   }
+}
+
+############################################
+# ATTACH EC2 TO TARGET GROUP
+############################################
+
+resource "aws_lb_target_group_attachment" "ec2_attachment" {
+  target_group_arn = aws_lb_target_group.transaction_tracker_tg.arn
+  target_id        = var.ec2_instance_id
+  port             = 8080
+}
+
+############################################
+# OUTPUTS
+############################################
+
+output "alb_dns_name" {
+  value = aws_lb.transaction_tracker_alb.dns_name
 }
